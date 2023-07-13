5d8fcb7a7691553ad016fce1dc159a2862365193
th3heaters <Anil.leo04@gmail.com>
2023-07-14 02:47:51 +0300

    english

--- TestTask/main.cpp
+++ TestTask/main.cpp
@@ -2,9 +2,9 @@
 int main()
 {
 	std::string input, output;
-	std::cout << "âõîäíîé ôàéë: ";
+	std::cout << "input tape file: ";
 	std::cin >> input;
-	std::cout << "âûõîäíîé ôàéë ";
+	std::cout << "output tape file: ";
 	std::cin >> output;
 	tapes t(input, output);
 	t.init_tape();
